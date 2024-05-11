92f5049f315fb320fdca716f188d48cbf5fd6d55
esot0001 <esot0001@algonquinlive.com>
2024-05-10 23:58:59 -0400

    Displayed fetched API data on DOM using a table

--- script.js
+++ script.js
@@ -13,17 +13,31 @@ function charactersLeft(){
     if(charactersLeft<0){
         charactersLeftEl.textContent = 0;
     }
+
 }
 
 textareaEl.addEventListener('input', charactersLeft);
 
 //fetch and display company feedback data from api
+// fetch('https://bytegrad.com/course-assets/js/1/api/feedbacks')
+// .then(response => {return response.json();})
+// .then(data => {
+//     data.feedbacks.forEach(company => {
+//         const htmlMarkup = `<li class="feedback__item">${company.upvoteCount} ${company.badgeLetter} ${company.company} ${company.text} ${company.daysAgo} </li>`;
+//         document.querySelector('.feedbacks').insertAdjacentHTML('afterend', htmlMarkup);
+//     });
+// })
+// .catch(function(error){console.log("Error: " + error)});
+
+//fetch and display company feedback data using tables
 fetch('https://bytegrad.com/course-assets/js/1/api/feedbacks')
 .then(response => {return response.json();})
 .then(data => {
     data.feedbacks.forEach(company => {
-        const htmlMarkup = `<li class="feedback__item">${company.upvoteCount} ${company.badgeLetter} ${company.company} ${company.text} ${company.daysAgo} </li>`;
-        document.querySelector('.feedbacks').insertAdjacentHTML('afterend', htmlMarkup);
+        const htmlMarkup = ` <tr class="feedback__row"><td class="feedback__row--upvote">${company.upvoteCount}</td><td class="feedback__row--badgeLetter">${company.badgeLetter}</td>
+        <td class="feedback__row--text">${company.company} ${company.text}</td>
+        <td class="feedback__row--daysAgo">${company.daysAgo}</td></tr>`;
+        document.querySelector('.feedback__row').insertAdjacentHTML('afterend', htmlMarkup);
     });
 })
 .catch(function(error){console.log("Error: " + error)});
@@ -33,3 +47,10 @@ fetch('https://bytegrad.com/course-assets/js/1/api/feedbacks')
 // .then(response => {return response.json()})
 // .then(data => console.log(data))
 // .catch(function(error){console.log("Error: " + error)});
+
+// when submit is clicked check if # was used in input
+//if yes, highlight textarea border green for success and submit form, and display data on DOM
+//if no highlight red for fail
+
+//when hashtag buttons are clicked filter data from API to display only those items
+//on hover effect for #buttons
