d0cd770de4b28c2d001ba7342daa7f3b6b7d9545
esot0001 <esot0001@algonquinlive.com>
2024-05-13 07:55:39 -0400

    Submitted hardcoded data to API

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const charactersLeftEl = document.querySelector('.textarea__textarea--characters
 const companyListEl = document.querySelector('.company');
 
 const listCompanyEl = document.querySelector('.company__name');
-const hashtag = document.querySelector('.company__name--hashtag');
+const hashtagName = document.querySelector('.company__name--hashtag');
 
 
 
@@ -65,18 +65,32 @@ const submitFormEl = document.querySelector('#submitForm');
 
 async function sendFormData(){
     //associate the FormData object with the form element
-    const formData = new FormData(submitFormEl); //can .get, set, delete, append data from form
-    console.log(formData.get('text'));
-    const data = Object.fromEntries(formData);
+    // const formData = new FormData(submitFormEl); //methods = get, set, delete, append data from form
+    // const data = Object.fromEntries(formData);
+
+    //PROBLEM WAS TRYING TO USE FORMDATA INSTEAD OF TEXTAREA VALUE
+    console.log(textareaEl.value);
+
+    //object to return to API
+    const feedbackInput = {
+        company: "Test",
+        badgeLetter: "T",
+        upvoteCount: 0,
+        daysAgo: 0,
+        text: "Testing POST"
+    }
+
+    console.log(feedbackInput);
+
     try{
-        const response = await fetch('https://reqres.in/api/users',{
+        const response = await fetch('https://bytegrad.com/course-assets/js/1/api/feedbacks',{
             method: "POST",
             //specify content type
             headers:{
                 'Content-Type': 'application/json'
             },
             //set the FormData instance as the request body
-            body: JSON.stringify(data),
+            body: JSON.stringify(feedbackInput),
         });
         console.log(await response.json());
     } catch(error){
@@ -139,15 +153,29 @@ function onclickHandler(company){
 }
 
 companyListEl.addEventListener('click', function(event){
-    console.log(event.target);
+    console.log(event.target); //returned button tag == same output as console.log(hashtagName)
+    // console.log(event.target.value);
+    
 
-    //get the name of company
-    const clickedCompany = event.target;
+    // //get the name of company
+    // const clickedCompany = event.target;
+    // const companyName = companyListEl.textContent;
+    // console.log(companyName); //returned all company names
 
-    //use name of company to filter data
+    // const clickedCompanyName = listCompanyEl.textContent;
+    // console.log(clickedCompanyName); //returned ByteGrad
+
+    // const name = clickedCompany.textContent;
+    // console.log(typeof clickedCompany ); //object
+
+    // //use name of company to filter data
+    // console.log(hashtagName); //returned button tag of first element
+    // console.log(typeof hashtagName); //object
+    // console.log(hashtagName.textContent);
 
     //display filtered result
 
 });
 
 //display data list in descending order of upvote
+//the fetched data is already sorted. Check CSS to display it in reverse column
